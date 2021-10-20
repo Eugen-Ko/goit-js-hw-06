94d71de4fd2c78d68c61e66a6ff1c869e68f6a2c
Eugen-Ko <kovalchuk.eak@gmail.com>
2021-10-20 11:16:12 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const arrayTag = [];
+
+ingredients.forEach(element => {arrayTag.push(document.createElement("li").textContent = " " + element)})
+
+// Здесь элементы передаются одной строкой и выводятся в одну строку в HTML
+// Не могу придумать как при такой записи сделать перенос на следующую строку.
+document.querySelector("#ingredients").append([...arrayTag]);
+
+// -------------------------------------------
+// Это просто разделитель
+document.querySelector("#ingredients").append(document.createElement("p"));
+// -------------------------------------------
+
+
+//Что бы элементы располагались в столбик, как показано ниже.
+arrayTag.forEach(element => {
+  document.querySelector("#ingredients").append(document.createElement("br"));
+  document.querySelector("#ingredients").append(element);
+});
