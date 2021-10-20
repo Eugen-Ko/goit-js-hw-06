11cc6d6ebc3504e29479b0b1b543afcb4a736d28
Eugen-Ko <kovalchuk.eak@gmail.com>
2021-10-20 16:46:04 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,26 @@
+const btnDecrement = document.querySelector("[data-action='decrement']");
+const btnIncrement = document.querySelector("[data-action='increment']");
+
+const value = document.querySelector("#value");
+
+let counterValue = Number(value.textContent);
+
+// btnDecrement.addEventListener("click", () => {
+//   counterValue -= 1;
+//   value.textContent = counterValue;
+// });
+
+btnDecrement.addEventListener("click", () => {
+  counterValue -= 1;
+  value.textContent = counterValue;
+});
+
+btnIncrement.addEventListener("click", () => {
+  counterValue += 1;
+  value.textContent = counterValue;
+});
+
+// Нужно доработать - не красиво.
+
+
+
