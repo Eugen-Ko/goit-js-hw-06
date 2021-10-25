7c58b8965632d775b1a942a0e39dba9153d19643
Eugen-Ko <kovalchuk.eak@gmail.com>
2021-10-25 15:46:17 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,7 +1,19 @@
+// Функция генератора случайного цвета
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+// Инициализация узлов
 const changeBodyColor = document.querySelector("body");
+const btnChange = document.querySelector(".change-color");
+const outputColor = document.querySelector(".color");
 
-console.log(changeBodyColor.style.backgroundColor);
+// Изменение цвета фона и вывод номера цвета в спан.
+const changeBgColor = () => {
+  changeBodyColor.style.backgroundColor = getRandomHexColor()
+  outputColor.textContent = changeBodyColor.style.backgroundColor;
+}
+
+// Слушатель
+btnChange.addEventListener("click", changeBgColor); 
+
