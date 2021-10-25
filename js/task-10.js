75fefd1218b125333facbc5debb51c124152223c
Eugen-Ko <kovalchuk.eak@gmail.com>
2021-10-25 17:05:18 +0300

    111

--- js/task-10.js
+++ js/task-10.js
@@ -7,4 +7,23 @@ function getRandomHexColor() {
 const amountField = document.querySelector("#controls input");
 const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
+const boxesPlace = document.querySelector("#boxes");
+
+let counter = 0;
+
+const createBoxes = (amount) => {
+  console.log(amount);
+  for (let i = 0; i <= amount; i += 1) {
+    boxesPlace.insertAdjacentHTML("beforeend",`<div  style="width : ${i * 10 + 30}px; height : ${i * 10 + 30}px; background-color: ${getRandomHexColor()}"></div>`)
+  }
+}
+
+
+
+createBtn.addEventListener("click", createBoxes(Number(amountField.textContent)));
+// createBtn.addEventListener("click", destroyBox());
+
+
+
+
 
