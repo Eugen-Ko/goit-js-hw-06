7592cc09ae3b96f66a3394e5b83746081089e863
Eugen-Ko <kovalchuk.eak@gmail.com>
2021-10-26 16:15:39 +0300

    11

--- js/task-10.js
+++ js/task-10.js
@@ -9,22 +9,34 @@ const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
 const boxesPlace = document.querySelector("#boxes");
 
-createBtn.addEventListener("click", createBoxes(amountField.value));
+let counter = 0;
 
-function createBoxes(amount) {
-  console.log(amount);
-
-  console.log("ggggggggggggggggg");
-  return;
+const createBoxes = () => {
+  for (let i = 1; i <= amountField.value; i += 1) {
+    boxesPlace.insertAdjacentHTML("beforeend", `<div  class="box${i}" style="width : ${i * 10 + 30}px; height : ${i * 10 + 30}px; background-color: ${getRandomHexColor()}"></div>`)
+  }
+  counter + amountField.value <= 100 ? counter += amountField.value : counter = 100;
 }
+
+const destroyBoxes = () => {
+  for (let i = amountField.value; i > 0; i -= 1) {
+    document.querySelector(`.box${i}`).remove();
+  }
+  counter - amountField.value >= 0 ? counter -= amountField.value : counter = 0;
+}
+
+  createBtn.addEventListener("click", createBoxes);
+  destroyBtn.addEventListener("click", destroyBoxes);
+
+
 
 // let counter = 0;
 
 // const createBoxes = (amount) => {
 //   console.log(amount);
-//   //   for (let i = 0; i <= amount; i += 1) {
-//   //     boxesPlace.insertAdjacentHTML("beforeend",`<div  style="width : 30px; height : 30px; background-color: ${getRandomHexColor()}"></div>`)
-//   //   }
+    // for (let i = 0; i <= amount; i += 1) {
+    //   boxesPlace.insertAdjacentHTML("beforeend",`<div  style="width : 30px; height : 30px; background-color: ${getRandomHexColor()}"></div>`)
+    // }
 // };
 
 // createBtn.addEventListener("click", console.log(amountField.valueAsNumber));
