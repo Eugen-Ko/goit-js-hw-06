5ff38e77c0b21c14865ebd38f46f2e673d801df2
Eugen-Ko <kovalchuk.eak@gmail.com>
2021-11-29 11:48:09 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,30 +1,51 @@
-// Инициализация узлов
-const btnDecrement = document.querySelector("[data-action='decrement']");
-const btnIncrement = document.querySelector("[data-action='increment']");
-const value = document.querySelector("#value");
 
-// Мой вариант -------------------------------------
-// Слушатели + сразу обработка события 
-// работает с любым начальным значением, заданным в HTML.
-// 
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+let valueSpan = document.querySelector('#value');
+let counterValue = 0;
+decrementBtn.addEventListener("click", () => {
+    counterValue -= 1;
+    valueSpan.textContent = counterValue;
+    console.log(valueSpan);
+});
+incrementBtn.addEventListener("click", () => {
+    counterValue += 1;
+    valueSpan.textContent = counterValue;
+    console.log(valueSpan);
+ });
+
+
+
+
+
+
+// // Инициализация узлов
+// const btnDecrement = document.querySelector("[data-action='decrement']");
+// const btnIncrement = document.querySelector("[data-action='increment']");
+// const value = document.querySelector("#value");
+
+// // Мой вариант -------------------------------------
+// // Слушатели + сразу обработка события 
+// // работает с любым начальным значением, заданным в HTML.
+// // 
+// // btnDecrement.addEventListener("click", () => {
+// //   value.textContent = Number(value.textContent) - 1;
+// // });
+// // btnIncrement.addEventListener("click", () => {
+// //   value.textContent = Number(value.textContent) + 1;
+// // });
+// // ----------------------------------------------------
+
+// // Вариант по условию задачи ----------------------
+// let counterValue = 0;
+
 // btnDecrement.addEventListener("click", () => {
-//   value.textContent = Number(value.textContent) - 1;
+//   counterValue -= 1;
+//   value.textContent = counterValue;
 // });
 // btnIncrement.addEventListener("click", () => {
-//   value.textContent = Number(value.textContent) + 1;
+//   counterValue += 1;
+//   value.textContent = counterValue;
 // });
-// ----------------------------------------------------
-
-// Вариант по условию задачи ----------------------
-let counterValue = 0;
-
-btnDecrement.addEventListener("click", () => {
-  counterValue -= 1;
-  value.textContent = counterValue;
-});
-btnIncrement.addEventListener("click", () => {
-  counterValue += 1;
-  value.textContent = counterValue;
-});
 
 
